3debac3b5abb9f4518ffbbbf4a065f799c5b8bfb
chknwaffles <wonsae.kim@gmail.com>
2019-07-18 21:59:25 -0400

    add click handler for game menu canvas

--- src/components/GameMenu.js
+++ src/components/GameMenu.js
@@ -1,41 +1,60 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import '../stylesheets/Game.css'
 
 export default function GameMenu(props) {
-    const { changeStatus } = props
+    const { changeStatus, changePage } = props
+    const [paths, setPaths] = useState([])
     const canvasRef = useRef(null)
     const buttonSize = [430, 150]
-
+    
     //componentdidmount
     useEffect(() => {
         const canvas = canvasRef.current
         const context = canvas.getContext('2d')
-        const playButton = () => {
-            context.strokeRect(75, 60, buttonSize[0], buttonSize[1])
-            context.font = '50px Helvetica'
-            context.fillText('play now', 200, 150)
-        }
-        const loginButton = () => {
-            context.strokeRect(75, 240, buttonSize[0], buttonSize[1])
-            context.font = '50px Helvetica'
-            context.fillText('login', 200, 330)
-        }
-        const signUpButton = () => {
-            context.strokeRect(75, 420, buttonSize[0], buttonSize[1])
-            context.font = '50px Helvetica'
-            context.fillText('signup', 200, 500)
-        }
 
-        playButton()
-        loginButton()
-        signUpButton()
+        //create paths to check for clicks
+        const playPath = new Path2D()
+        const loginPath = new Path2D()
+        const signUpPath = new Path2D()
+        
+        //render each button in order: play, login, signup
+        playPath.rect(75, 60, buttonSize[0], buttonSize[1])
+        context.strokeStyle = 'red'
+        context.strokeRect(75, 60, buttonSize[0], buttonSize[1])
+        context.font = '50px Helvetica'
+        context.fillText('play now', 200, 150)
+
+        loginPath.rect(75, 240, buttonSize[0], buttonSize[1])
+        context.strokeStyle = 'green'
+        context.strokeRect(75, 240, buttonSize[0], buttonSize[1])
+        context.font = '50px Helvetica'
+        context.fillText('login', 200, 330)
+
+        signUpPath.rect(75, 420, buttonSize[0], buttonSize[1])
+        context.strokeStyle = 'yellow'
+        context.strokeRect(75, 420, buttonSize[0], buttonSize[1])
+        context.font = '50px Helvetica'
+        context.fillText('signup', 200, 500)
+
+        // set paths array
+        setPaths([playPath, loginPath, signUpPath])
     }, [])
 
     const handleClick = (e) => {
-        const targetButton = canvasRef.current.getBoundingClientRect(),
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
+        const targetButton = canvas.getBoundingClientRect(),
                 x = e.clientX - targetButton.left,
                 y = e.clientY - targetButton.top
 
+        paths.forEach((path, i) => {
+            if (context.isPointInPath(path, x, y)) {
+                console.log('Path' + (i + 1) + ' clicked')
+                switch(i) {
+                    case 0: changePage('play'); break;
+                }
+            }
+        })
     }
 
     return (
--- src/containers/GameContainer.js
+++ src/containers/GameContainer.js
@@ -4,13 +4,23 @@ import StatusBar from '../components/StatusBar'
 import Game from './Game'
 
 export default function GameContainer() {
+    const [page, setPage] = useState('')
     const [status, setStatus] = useState(false)
 
     const changeStatus = () => setStatus(true)
+    const changePage = (newPage) => setPage(newPage)
 
+    const renderPage = () => {
+        switch(page) {
+            case '': return <GameMenu changePage={changePage} />
+            case 'play': return <Game />
+            default: break;
+        }
+    }
     return (
         <React.Fragment>
-            {status ? <Game /> : <GameMenu changeStatus={changeStatus} />}
+            {renderPage()}
+            {/* {status ? <Game /> : <GameMenu changeStatus={changeStatus} changePage={changePage} />} */}
             <StatusBar />
         </React.Fragment>
     )
