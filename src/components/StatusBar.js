652bb06a284faa562a92ac93703cb30d3aecfd31
chknwaffles <wonsae.kim@gmail.com>
2019-07-31 06:51:43 -0400

    statusbar

--- src/components/StatusBar.js
+++ src/components/StatusBar.js
@@ -1,11 +1,27 @@
 import React, { useEffect, useRef } from 'react'
+import bomb from '../images/bombicon.png'
+import fire from '../images/firepowerupicon.png'
 
-export default function StatusBar() {
+export default function StatusBar(props) {
+    const { players } = props
     const canvasRef = useRef(null)
 
     useEffect(() => {
+        const canvas = canvasRef.current
+        const context = canvas.getContext('2d')
 
-    }, [])
+        context.font = '50px Helvetica'
+        
+        const renderImages = () => {
+            const image = new Image()
+            image.src = source
+            image.onload = () => {
+                context.drawImage(image, 0 * 50, 0 * 50)
+            }
+        }
+
+        renderImages(row, col, source)
+    }, [players])
 
     return (
         <canvas ref={canvasRef} className='game' width={650} height={50} />
