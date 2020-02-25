ac97b14e4597cce7824f115b1391fa2a68c17b5d
chknwaffles <wonsae.kim@gmail.com>
2020-02-25 17:59:35 -0500

    more fixes!

--- src/components/GameContainer.js
+++ src/components/GameContainer.js
@@ -5,6 +5,8 @@ import WaitingRoom from './WaitingRoom'
 import EndGame from './EndGame'
 import Game from './Game'
 
+var backendUrl = process.env.BACKEND_URL || 'http://localhost:4000'
+
 export default function GameContainer(props) {
     const { user, changePage } = props
     const [status, setStatus] = useState('')
